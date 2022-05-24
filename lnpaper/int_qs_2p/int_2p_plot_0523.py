d23ee4140f3fb643a1595876207a121f3062f02b
Xiaoyu Jiang <xjiang@DESKTOP-JJKTTKE.localdomain>
2022-05-23 21:34:07 -0500

    0523_l1

--- lnpaper/int_qs_2p/int_2p_plot_0523.py
+++ lnpaper/int_qs_2p/int_2p_plot_0523.py
@@ -12,7 +12,7 @@ h0=200
 print(3/5)
 tpi=1
 
-filestr="0519_2p_1.txt"
+filestr="intswp2_1.txt"
 f=open(filestr,"r")
 xa=[]
 ya=[]
@@ -23,33 +23,29 @@ omegar=2*np.pi*(1e6)
 h_g=200
 
 
-for i in range(7):
-    rtmp=f.readline()
-for i in range(7):
+for i in range(10):
     r=f.readline()
-    r=f.readline()
-    r=f.readline()
-    y=r.split()
-    print("y="+str(y))
+    x,y,sp,sn=r.split()
+    print(str(i)+" "+x+" "+y)
     xa.append(0.01*(i+1))
-    ya.append(1-float(y[0]))
-    spa.append(0)
-    sna.append(0)
+    ya.append(float(y))
+    spa.append(float(y)+float(sp))
+    sna.append(float(y)-float(sn))
 ##    if(i==40):
 ##        x=1
-    #fg=float(x)*(1e6)
+    fg=float(x)*(1e6)
 
     #print(2*omegag**2*sg/(omegar**2))
     N=1/2
     et=N**2*(np.pi)**2*(0.02*(i+1))**2/4
-    ta.append(1*et)
+    ta.append(2*et)
 
 
 plt.plot(xa,ya,'o-',label="Numerics",color='red')
 plt.plot(xa,ta,label="Theory",color='blue')
-#plt.plot(xa,spa,'r',alpha=0.2,label="Uncertainty")
-#plt.plot(xa,sna,'r',alpha=0.2)
-#plt.fill_between(xa,spa,sna,color='crimson',alpha=0.1)
+plt.plot(xa,spa,'r',alpha=0.2,label="Uncertainty")
+plt.plot(xa,sna,'r',alpha=0.2)
+plt.fill_between(xa,spa,sna,color='crimson',alpha=0.1)
 #plt.legend(loc="lower right", prop={'size': 10})
 plt.yscale('log')
 plt.text(0.01, 0.02, 'a', horizontalalignment='center',
@@ -62,7 +58,7 @@ f.close()
 
 plt.subplot(2,2,2)
 tpi=1
-filestr="0519_2p_1w.txt"
+filestr="intswp2w_1.txt"
 f=open(filestr,"r")
 xa=[]
 ya=[]
@@ -71,18 +67,16 @@ spa=[]
 sna=[]
 omegar=2*np.pi*(1e6)
 h_g=200
-for i in range(7):
-    rtmp=f.readline()
-for i in range(7):
+
+
+for i in range(10):
     r=f.readline()
-    r=f.readline()
-    r=f.readline()
-    y=r.split()
-    print("y="+str(y))
+    x,y,sp,sn=r.split()
+    print(str(i)+" "+x+" "+y)
     xa.append(0.01*(i+1))
-    ya.append(1-float(y[0]))
-    spa.append(0)
-    sna.append(0)
+    ya.append(float(y))
+    spa.append(float(y)+float(sp))
+    sna.append(float(y)-float(sn))
 ##    if(i==40):
 ##        x=1
     fg=float(x)*(1e6)
@@ -91,7 +85,7 @@ for i in range(7):
     N=1/2
     et=N**2*(np.pi)**2*(0.02*(i+1))**2/4
     ta.append(2*et)
-    r=f.readline()
+
 
 plt.plot(xa,ya,'o-',label="Numerics",color='red')
 plt.plot(xa,ta,label="Theory",color='blue')
@@ -113,7 +107,7 @@ h0=200
 print(3/5)
 tpi=2
 
-filestr="0519_2p_2.txt"
+filestr="intswp2_2.txt"
 f=open(filestr,"r")
 xa=[]
 ya=[]
@@ -122,18 +116,16 @@ spa=[]
 sna=[]
 omegar=2*np.pi*(1e6)
 h_g=200
-for i in range(7):
-    rtmp=f.readline()
+
+
 for i in range(10):
     r=f.readline()
-    r=f.readline()
-    r=f.readline()
-    y=r.split()
-    print("y="+str(y))
+    x,y,sp,sn=r.split()
+    print(str(i)+" "+x+" "+y)
     xa.append(0.01*(i+1))
-    ya.append(1-float(y[0]))
-    spa.append(0)
-    sna.append(0)
+    ya.append(float(y))
+    spa.append(float(y)+float(sp))
+    sna.append(float(y)-float(sn))
 ##    if(i==40):
 ##        x=1
     fg=float(x)*(1e6)
@@ -142,7 +134,7 @@ for i in range(10):
     N=1/1
     et=N**2*(np.pi)**2*(0.02*(i+1))**2/4
     ta.append(2*et)
-    r=f.readline()
+
 
 plt.plot(xa,ya,'o-',label="Numerics",color='red')
 plt.plot(xa,ta,label="Theory",color='blue')
@@ -161,7 +153,7 @@ f.close()
 
 plt.subplot(2,2,4)
 tpi=2
-filestr="0519_2p_2w.txt"
+filestr="intswp2w_2.txt"
 f=open(filestr,"r")
 xa=[]
 ya=[]
@@ -170,18 +162,16 @@ spa=[]
 sna=[]
 omegar=2*np.pi*(1e6)
 h_g=200
-for i in range(7):
-    rtmp=f.readline()
-for i in range(2):
+
+
+for i in range(10):
     r=f.readline()
-    r=f.readline()
-    r=f.readline()
-    y=r.split()
-    print("y="+str(y))
+    x,y,sp,sn=r.split()
+    print(str(i)+" "+x+" "+y)
     xa.append(0.01*(i+1))
-    ya.append(1-float(y[0]))
-    spa.append(0)
-    sna.append(0)
+    ya.append(float(y))
+    spa.append(float(y)+float(sp))
+    sna.append(float(y)-float(sn))
 ##    if(i==40):
 ##        x=1
     fg=float(x)*(1e6)
@@ -190,7 +180,6 @@ for i in range(2):
     N=1/1
     et=N**2*(np.pi)**2*(0.02*(i+1))**2/4
     ta.append(2*et)
-    r=f.readline()
 
 
 plt.plot(xa,ya,'o-',label="Numerics",color='red')
@@ -207,7 +196,7 @@ f.close()
 plt.xlabel("RIN")
 
 
-plt.savefig('int_2p_0508.png')
+plt.savefig('int_2p.pdf')
 
 
 plt.show()
