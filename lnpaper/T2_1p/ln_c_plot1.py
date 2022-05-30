5d2cbc7f82415df24b23c64721410402bfb7f700
Xiaoyu Jiang <xjiang@DESKTOP-JJKTTKE.localdomain>
2022-05-29 20:45:58 -0500

    0529_1

--- lnpaper/T2_1p/ln_c_plot1.py
+++ lnpaper/T2_1p/ln_c_plot1.py
@@ -6,11 +6,10 @@ matplotlib.rcParams.update({'font.size': 12})
 fig = plt.figure()
 fig.set_size_inches(3.375,3.375*1)
 
-data = np.loadtxt("ln_c_32_0426.txt")
-print(data)
-lw_arr=data[:,0]*1000
+data = np.loadtxt("ln1_0804.txt")
+lw_arr=data[:,0]/1000
 print(lw_arr)
-t2_arr=data[:,1]
+t2_arr=data[:,1]*1000000
 lwfit_arr=np.linspace(0,100,1000)
 
 def func(x,a):
@@ -19,23 +18,22 @@ def func(x,a):
 popt,pcov=curve_fit(func,lw_arr,t2_arr)
 aaa=popt[0]
 t2fit_arr=func(lwfit_arr,aaa)
+
 print(aaa)
 print(1000/(2*np.pi))
-theoconst=1000/(8*np.pi)
+theoconst=1000/(2*np.pi)
 t2theo_arr=func(lwfit_arr,theoconst)
-
 
 plt.clf()
 plt.plot(lw_arr,t2_arr,'ro',label="T2,numerics")
-#plt.plot(lwfit_arr,t2fit_arr,'g')
+#plt.plot(lwfit_arr,t2fit_arr,label="Fitting numerics to 1/x function")
 plt.plot(lwfit_arr,t2theo_arr,label="Theory")
 
 plt.xlabel("Linewidth/kHz")
 plt.ylabel("T2/μs")
 plt.title("T2-Linewidth")
-plt.legend(loc="upper right",prop={'size': 10})
-plt.xlim([0,110])
-plt.ylim([0,4])
-plt.savefig("T2_2p_0517.pdf",bbox_inches='tight',dpi=100)
+#plt.xlim([0,110])
+plt.legend(loc="upper right")
+plt.ylim([0,20])
+plt.savefig("T2_1p.pdf",bbox_inches='tight',dpi=100)
 plt.show()
-
---
+++ lnpaper/int_qs_2p/int_2p_plot_0414.py
@@ -0,0 +1,214 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+matplotlib.rcParams.update({'font.size': 12})
+
+
+plt.subplot(2,2,1)
+
+h0=200
+
+print(3/5)
+tpi=1
+
+filestr="0519_2p_1.txt"
+f=open(filestr,"r")
+xa=[]
+ya=[]
+ta=[]
+spa=[]
+sna=[]
+omegar=2*np.pi*(1e6)
+h_g=200
+for i in range(7):
+    r=f.readline()
+for i in range(8):
+    r=f.readline()
+    r=f.readline()
+    r=f.readline()
+    r=f.readline()
+    y=r.split()
+    print(y)
+    xa.append(0.01*(i+1))
+    ya.append(1-float(y[0]))
+    spa.append(0)
+    sna.append(0)
+##    if(i==40):
+##        x=1
+#    fg=float(x)*(1e6)
+
+    #print(2*omegag**2*sg/(omegar**2))
+    N=1/2
+    et=N**2*(np.pi)**2*(0.02*(i+1))**2/4
+    ta.append(2*et)
+
+
+plt.plot(xa,ya,'o-',label="Numerics",color='red')
+plt.plot(xa,ta,label="Theory",color='blue')
+plt.plot(xa,spa,'r',alpha=0.2,label="Uncertainty")
+plt.plot(xa,sna,'r',alpha=0.2)
+plt.fill_between(xa,spa,sna,color='crimson',alpha=0.1)
+#plt.legend(loc="lower right", prop={'size': 10})
+plt.yscale('log')
+plt.text(0.01, 0.02, 'a', horizontalalignment='center',
+     verticalalignment='center')
+
+plt.xlabel("RIN")
+plt.ylabel('Error')
+f.close()
+
+
+plt.subplot(2,2,2)
+tpi=1
+filestr="0519_2p_1w.txt"
+f=open(filestr,"r")
+xa=[]
+ya=[]
+ta=[]
+spa=[]
+sna=[]
+omegar=2*np.pi*(1e6)
+h_g=200
+for i in range(7):
+    r=f.readline()
+for i in range(7):
+    r=f.readline()
+    r=f.readline()
+    r=f.readline()
+    r=f.readline()
+    y=r.split()
+    print(y)
+    xa.append(0.01*(i+1))
+    ya.append(1-float(y[0]))
+    spa.append(0)
+    sna.append(0)
+##    if(i==40):
+##        x=1
+#    fg=float(x)*(1e6)
+
+    #print(2*omegag**2*sg/(omegar**2))
+    N=1/2
+    et=N**2*(np.pi)**2*(0.02*(i+1))**2/4
+    ta.append(2*et)
+
+
+plt.plot(xa,ya,'o-',label="Numerics",color='red')
+plt.plot(xa,ta,label="Theory",color='blue')
+plt.plot(xa,spa,'r',alpha=0.2,label="Uncertainty")
+plt.plot(xa,sna,'r',alpha=0.2)
+plt.fill_between(xa,spa,sna,color='crimson',alpha=0.1)
+#plt.legend(loc="lower right", prop={'size': 10})
+plt.yscale('log')
+plt.text(0.01, 0.02, 'c', horizontalalignment='center',
+     verticalalignment='center')
+f.close()
+
+plt.xlabel("RIN")
+#---------------------------------------------------------------------
+plt.subplot(2,2,3)
+
+h0=200
+
+print(3/5)
+tpi=2
+
+filestr="0519_2p_2.txt"
+f=open(filestr,"r")
+xa=[]
+ya=[]
+ta=[]
+spa=[]
+sna=[]
+omegar=2*np.pi*(1e6)
+h_g=200
+for i in range(7):
+    r=f.readline()
+for i in range(8):
+    r=f.readline()
+    r=f.readline()
+    r=f.readline()
+    r=f.readline()
+    y=r.split()
+    print(y)
+    xa.append(0.01*(i+1))
+    ya.append(1-float(y[0]))
+    spa.append(0)
+    sna.append(0)
+##    if(i==40):
+##        x=1
+#    fg=float(x)*(1e6)
+
+    #print(2*omegag**2*sg/(omegar**2))
+    N=1/1
+    et=N**2*(np.pi)**2*(0.02*(i+1))**2/4
+    ta.append(2*et)
+
+
+plt.plot(xa,ya,'o-',label="Numerics",color='red')
+plt.plot(xa,ta,label="Theory",color='blue')
+plt.plot(xa,spa,'r',alpha=0.2,label="Uncertainty")
+plt.plot(xa,sna,'r',alpha=0.2)
+plt.fill_between(xa,spa,sna,color='crimson',alpha=0.1)
+#plt.legend(loc="lower right", prop={'size': 10})
+plt.yscale('log')
+plt.text(0.01, 0.09, 'b', horizontalalignment='center',
+     verticalalignment='center')
+
+plt.xlabel("RIN")
+plt.ylabel('Error')
+f.close()
+
+
+plt.subplot(2,2,4)
+tpi=2
+filestr="0519_2p_1.txt"
+f=open(filestr,"r")
+xa=[]
+ya=[]
+ta=[]
+spa=[]
+sna=[]
+omegar=2*np.pi*(1e6)
+h_g=200
+for i in range(7):
+    r=f.readline()
+for i in range(4):
+    r=f.readline()
+    r=f.readline()
+    r=f.readline()
+    r=f.readline()
+    y=r.split()
+    print(y)
+    xa.append(0.01*(i+1))
+    ya.append(1-float(y[0]))
+    spa.append(0)
+    sna.append(0)
+##    if(i==40):
+##        x=1
+#    fg=float(x)*(1e6)
+
+    #print(2*omegag**2*sg/(omegar**2))
+    N=1/1
+    et=N**2*(np.pi)**2*(0.02*(i+1))**2/4
+    ta.append(2*et)
+
+
+plt.plot(xa,ya,'o-',label="Numerics",color='red')
+plt.plot(xa,ta,label="Theory",color='blue')
+plt.plot(xa,spa,'r',alpha=0.2,label="Uncertainty")
+plt.plot(xa,sna,'r',alpha=0.2)
+plt.fill_between(xa,spa,sna,color='crimson',alpha=0.1)
+plt.legend(loc="lower right", prop={'size': 10})
+plt.yscale('log')
+plt.text(0.01, 0.01, 'd', horizontalalignment='center',
+     verticalalignment='center')
+f.close()
+
+plt.xlabel("RIN")
+
+
+plt.savefig('int_2p.pdf')
+
+
+plt.show()
